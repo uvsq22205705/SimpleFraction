82cdcaf994b70668156f838d92245893f725b9ca
Ons BEN BRAHIM <onsbbr29@gmail.com>
2023-10-03 23:15:30 +0200

    1er commit

---
+++ Fraction.java
@@ -0,0 +1,12 @@
+public class Fraction {
+    private int numerateur;
+    private int denominateur;
+
+    public Fraction(int numerateur, int denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+    public String toString() {
+        return "Je suis une fraction.";
+    }
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Démarrage");
+
+        Fraction fraction = new Fraction(3, 4);
+        System.out.println(fraction.toString());
+    }
+}
