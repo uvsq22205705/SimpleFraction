24f571cba5d19312898c59989631e59bca41b554
Ons BEN BRAHIM <onsbbr29@gmail.com>
2023-10-04 11:51:17 +0200

    updates

--- Fraction.java
+++ Fraction.java
@@ -1,12 +1,105 @@
-public class Fraction {
-    private int numerateur;
-    private int denominateur;
 
+public class Fraction extends Number implements Comparable<Fraction>{
+    // Déclaration des attributs
+    private final int numerateur;
+    private final int denominateur;
+    // Déclaration des constantes
+    final static int ZERO_NUM = 0;
+    final static int ZERO_DENOM = 1;
+    final static int UN_NUM = 1;
+    final static int UN_DENOM = 1;
+
+    // Constructeur avec numérateur et dénominateur
     public Fraction(int numerateur, int denominateur) {
         this.numerateur = numerateur;
         this.denominateur = denominateur;
     }
+
+    // Getter pour le numérateur
+    public int getNumerateur() {
+        return numerateur;
+    }
+
+    // Getter pour le dénominateur
+    public int getDenominateur() {
+        return denominateur;
+    }
+
+    // Méthode pour obtenir la valeur en virgule flottante
+    public double doubleValue() {
+        return (double) numerateur / denominateur;
+    }
+
+    // Méthodes de l'interface Number à implémenter
+    @Override
+    public int intValue() {
+        return numerateur / denominateur;
+    }
+
+    @Override
+    public long longValue() {
+        return (long) numerateur / denominateur;
+    }
+
+    @Override
+    public float floatValue() {
+        return (float) numerateur / denominateur;
+    }
+
+    @Override
+    public short shortValue() {
+        return (short) (numerateur / denominateur);
+    }
+
+    @Override
+    public byte byteValue() {
+        return (byte) (numerateur / denominateur);
+    }
+
+    // Constructeur avec juste le numérateur (dénominateur égal à 1)
+    public Fraction(int numerateur) {
+        this.numerateur = numerateur;
+        this.denominateur = 1;
+    }
+
+    // Constructeur sans argument (numérateur égal à 0 et dénominateur égal à 1)
+    public Fraction() {
+        this.numerateur = 0;
+        this.denominateur = 1;
+    }
+
     public String toString() {
-        return "Je suis une fraction.";
+        //return "Je suis une fraction.";
+        return numerateur + "/" + denominateur;
+    }
+
+    // Méthode pour additionner deux fractions
+    public Fraction add(Fraction other) {
+        int commonDenominator = this.denominateur * other.denominateur;
+        int sumNumerator = (this.numerateur * other.denominateur) + (other.numerateur * this.denominateur);
+        return new Fraction(sumNumerator, commonDenominator);
+    }
+
+    // Méthode pour tester l'égalité entre fractions
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true; // Les références sont identiques, les fractions sont égales
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false; // L'objet n'est pas une instance de Fraction
+        }
+        Fraction other = (Fraction) obj;
+        return this.numerateur * other.denominateur == other.numerateur * this.denominateur;
+    }
+
+    // Méthode pour comparer deux fractions selon l'ordre naturel
+    @Override
+    public int compareTo(Fraction other) {
+        int thisNumeratorTimesOtherDenominator = this.numerateur * other.denominateur;
+        int otherNumeratorTimesThisDenominator = other.numerateur * this.denominateur;
+
+        // Compare les deux valeurs en multipliant par le dénominateur commun
+        return Integer.compare(thisNumeratorTimesOtherDenominator, otherNumeratorTimesThisDenominator);
     }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,72 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main (String[] args){
         System.out.println("Démarrage");
 
-        Fraction fraction = new Fraction(3, 4);
-        System.out.println(fraction.toString());
+        //Test de la méthode toString
+        //Fraction fraction = new Fraction(3, 4);
+        //System.out.println(fraction.toString()); // Affiche: Je suis une fraction.
+
+        // Test du constructeur avec numérateur et dénominateur
+        Fraction fraction1 = new Fraction(3, 4);
+        assert fraction1.toString().equals("3/4");
+
+        // Test du constructeur avec juste le numérateur (dénominateur égal à 1)
+        Fraction fraction2 = new Fraction(2);
+        assert fraction2.toString().equals("2/1");
+
+        // Test du constructeur sans argument (numérateur égal à 0 et dénominateur égal à 1)
+        Fraction fraction3 = new Fraction();
+        assert fraction3.toString().equals("0/1");
+
+        // Création d'objets Fraction avec les constantes
+        Fraction zero = new Fraction(Fraction.ZERO_NUM, Fraction.ZERO_DENOM); //Fraction zero = new Fraction(0,0)
+        Fraction un = new Fraction(Fraction.UN_NUM, Fraction.UN_DENOM); //Fraction un = newFraction(1,1)
+
+        // Utilisation des getters pour obtenir le numérateur et le dénominateur
+        System.out.println("ZERO Numérateur : " + zero.getNumerateur());
+        System.out.println("ZERO Dénominateur : " + zero.getDenominateur());
+        System.out.println("UN Numérateur : " + un.getNumerateur());
+        System.out.println("UN Dénominateur : " + un.getDenominateur());
+
+        // Utilisation de doubleValue() pour obtenir la valeur en virgule flottante
+        double zeroValeur = zero.doubleValue();
+        double uneValeur = un.doubleValue();
+
+        // Assertions pour tester la conversion
+        assert zeroValeur == 0.0 : "Erreur : zeroValeur ne devrait pas être différent de 0.0";
+        assert uneValeur == 1.0 : "Erreur : oneValeur ne devrait pas être différent de 1.0";
+
+        System.out.println("ZERO en valeur double : " + zeroValeur);
+        System.out.println("UN en valeur double : " + uneValeur);
+
+        // Addition de deux fractions
+        Fraction result = zero.add(un);
+
+        // Assertions pour tester l'addition
+        assert result.getNumerateur() == 1 : "Erreur : le numérateur du résultat devrait être égal à 1";
+        assert result.getDenominateur() == 1 : "Erreur : le dénominateur du résultat devrait être égal à 1";
+        System.out.println("Résultat de l'addition : " + result.getNumerateur() + "/" + result.getDenominateur());
+
+        // Test d'égalité entre fractions
+        assert zero.equals(new Fraction(0, 1)) : "Erreur : zero devrait être égal à 0/1";
+        assert un.equals(new Fraction(1, 1)) : "Erreur : one devrait être égal à 1/1";
+        System.out.println("Test d'égalité réussi !");
+
+        // Test de comparaison entre fractions
+        assert zero.compareTo(new Fraction(0, 1)) == 0 : "Erreur : zero est égal à 0/1";
+        assert un.compareTo(new Fraction(1, 1)) == 0 : "Erreur : one est égal à 1/1";
+        assert zero.compareTo(un) < 0 : "Erreur : zero est inférieur à one";
+        System.out.println("Test de comparaison réussi !");
+
+        // Test avec les nombres
+        Number aNumber = BigDecimal.ONE;
+        Number anotherNumber = new Fraction(1, 2);
+        double resultat = aNumber.doubleValue() + anotherNumber.doubleValue();
+        // Vérification
+        assert Math.abs(resultat - 1.5) < 1E-8 : "Erreur : la somme n'est pas égale à 1.5";
+        System.out.println("Test réussi !");
+
     }
 }
